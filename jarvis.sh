8ae06bfc53f6bc2ca4cb421e3a4ad56f1056405a
flotschilein <florianbraune1811@gmail.com>
2025-08-29 22:43:36 +0200

    lets go
    linux

--- jarvis.sh
+++ jarvis.sh
@@ -7,9 +7,10 @@ urls=(
     "https://chat.deepseek.com/"
 )
 
-code_folder="path to code folder"
+code_folder="/homeofcode"
 vscode_cmd="code"
-chrome_cmd="flatpak run com.google.Chrome"-
+chrome_cmd="flatpak run com.google.Chrome"
+
 $chrome_cmd --new-window "${urls[@]}" >/dev/null 2>&1 &
-$vscode_cmd" "$code_folder" >/dev/null 2>&1 &
+$vscode_cmd "$code_folder" >/dev/null 2>&1 &
 echo "that's it, ready to go!"
